a81584b455869ce4be00fc5415d180312c04a294
kirteshnuwal <kirteshnuwal@gmail.com>
2026-09-02 23:43:08 +0530

    Create Day_8.c

---
+++ Day_8.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    int matrix[3][4] = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12}
+    };
+
+    int top = 0, bottom = 2;
+    int left = 0, right = 3;
+
+    while (top <= bottom && left <= right) {
+
+        for (int i = left; i <= right; i++)
+            printf("%d ", matrix[top][i]);
+        top++;
+
+        for (int i = top; i <= bottom; i++)
+            printf("%d ", matrix[i][right]);
+        right--;
+
+        if (top <= bottom) {
+            for (int i = right; i >= left; i--)
+                printf("%d ", matrix[bottom][i]);
+            bottom--;
+        }
+
+        if (left <= right) {
+            for (int i = bottom; i >= top; i--)
+                printf("%d ", matrix[i][left]);
+            left++;
+        }
+    }
+
+    return 0;
+}
